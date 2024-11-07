33daed0247cf025edb99bdcb875f850b490ee9e3
조형석 <chs9902@naver.com>
2024-11-07 17:30:06 +0900

    :sparkles: feat: 사용자 상품 입력 기능 추가

--- src/main/kotlin/store/view/Input.kt
+++ src/main/kotlin/store/view/Input.kt
@@ -1,4 +1,14 @@
 package store.view
 
+import camp.nextstep.edu.missionutils.Console
+import store.util.Constants
+
+
 class Input {
+    fun readItem(stock:String):String{
+        println(Constants.INTRODUCE)
+        println(stock)
+        println(Constants.QUESTION_INPUT_ITEM)
+        return Console.readLine()
+    }
 }
